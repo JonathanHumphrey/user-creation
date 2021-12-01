19dc0de5d93ef520766b1859bc426e480d94b622
Joshua Humphrey <joshhumphrey1@gmail.com>
2021-12-01 00:29:34 -0600

    refactored formContainer to use reuseable forms

--- src/components/forms/FormTemplate.vue
+++ src/components/forms/FormTemplate.vue
@@ -1,60 +1,23 @@
 <template>
-  <div class="form-container">
-    <v-card-text
-      class="text-left"
-      v-for="item in formData"
-      v-bind:key="item.id"
-    >
-      <!-- Loops through the form data and renders each section -->
-      <div>
-        <p>{{ item.sectionName }}</p>
-        <div
-          class="input-block"
-          v-for="field in item.fields"
-          v-bind:key="field.id"
-        >
-          <!-- Then we loop through and render each field in that section -->
-          <!-- If the field has type "input" an input is rendered, else we render and text area -->
+  <form id="id">
+    <div class="formClass" v-show="show">
+      <h3>{{ formTitle }}</h3>
+      <div v-for="item in formData" v-bind:key="item.id" class="form">
+        <p class="section-name">{{ item.sectionName }}</p>
+        <div v-for="field in item.fields" v-bind:key="field.id">
           <div v-if="field.type === 'input'">
-            <v-text-field
-              v-bind:label="field.label"
+            <input
+              type="field.type"
               v-bind:placeholder="field.label"
               v-model="field.value"
-              outlined
-              class="mt-4"
-            >
-            </v-text-field>
-          </div>
-          <div v-else-if="field.type === 'checkbox'">
-            <v-checkbox v-model="field.value" v-bind:label="field.label">
-            </v-checkbox>
-          </div>
-          <div v-else>
-            <v-textarea
-              v-bind:name="field.label"
-              outlined
-              v-bind:label="field.label"
-              v-model="field.value"
-            >
-            </v-textarea>
+              class="input"
+            />
           </div>
         </div>
       </div>
-    </v-card-text>
-    <v-card-actions class="mx-5 pt-0">
-      <v-row justify="start" class="mb-4">
-        <v-btn
-         class="white--text mr-2"
-          data-testid="saveButton"
-          color="actionBlue"
-          @click="constructState(formData)"
-        >
-          Save
-        </v-btn>
-        <v-btn class="black--text mr-2" v-if="cancelFunction" @click="cancelFunction()"> Cancel </v-btn>
-      </v-row>
-    </v-card-actions>
-  </div>
+      <button class="submit" @click="constructState()">Submit</button>
+    </div>
+  </form>
 </template>
 <script>
 export default {
@@ -67,15 +30,14 @@ export default {
         }
       }
       let sender = {
-        data: newState
+        data: newState,
       };
       this.buttonFunction(sender);
-      if(this.cancelFunction) {
+      if (this.cancelFunction) {
         this.cancelFunction();
       }
-     
     },
   },
-  props: ["formData", "cancelFunction", "buttonFunction", "sendTo"],
+  props: ["id", "formClass", "show", "formTitle", "formData", "submitFunction"],
 };
 </script>
